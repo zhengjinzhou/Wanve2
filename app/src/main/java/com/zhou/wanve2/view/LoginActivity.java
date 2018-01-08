14000809e1490e4c7ac90da779bd4e56aaaef953
zhengjinzhou <1457594845@qq.com>
2018-01-08 11:06:53 +0800

    修改登录界面状态栏

--- app/src/main/java/com/zhou/wanve2/view/LoginActivity.java
+++ app/src/main/java/com/zhou/wanve2/view/LoginActivity.java
@@ -2,6 +2,8 @@ package com.zhou.wanve2.view;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Xml;
@@ -78,6 +80,11 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void init() {
+        if(Build.VERSION.SDK_INT >= 21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    |View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         initCheckBox();
         initInfo();
     }
